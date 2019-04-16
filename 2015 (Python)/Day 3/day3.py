5e692684952a3b28fb69c6662c71d6d15f9f40b4
Mark Berentsen <markberentsen@hotmail.com>
2019-04-16 20:22:34 +0200

    2015 - Day 3 done

--- 2015 (Python)/Day 3/day3.py
+++ 2015 (Python)/Day 3/day3.py
@@ -6,19 +6,30 @@ f = open(os.path.join(root, "input.txt"))
 input = f.read()
 f.close()
 
+# Set this to true for the first star
+star = False
+
+# updates the given position
+def UpdatePos(pos, sign):
+    return tuple([a + b for a, b in zip(pos, coordsDict[sign])])
+
 # a dictionary to get the coordinate increase depending on the sign
 coordsDict = {'^' : (0, 1), '>' : (1, 0), 'v' : (0, -1), '<' : (-1, 0)}
 
 # initialize the starting position and a set to keep all the visited coords in
-currPos = (0, 0)
-visitedPos = {currPos}
+currSantaPos, currRobotPos = (0, 0), (0, 0)
+visitedPos = {currSantaPos}
 
 # loop over the input, update the current position, add to visited list if not visited before
-for c in input:
-    currPos = tuple([a + b for a, b in zip(currPos, coordsDict[c])])
-    if currPos not in visitedPos:
-        visitedPos.add(currPos)
+for index, c in enumerate(input):
+    if index % 2 == 0 and not star:
+        currSantaPos = UpdatePos(currSantaPos, c)
+        if currSantaPos not in visitedPos:
+            visitedPos.add(currSantaPos)
+    else:
+        currRobotPos = UpdatePos(currRobotPos, c)
+        if currRobotPos not in visitedPos:
+            visitedPos.add(currRobotPos)
 
-# print star 1
+# print star 
 print("The amount of houses santa vistied is: " + str(len(visitedPos)))
-
